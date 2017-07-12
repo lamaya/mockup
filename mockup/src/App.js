d2557762c0c2009dc790ad13cb82c58a9cadcbb3
Laura Amayagit config --global user.email amaya.lauraa@gmail.com <amaya.lauraa@gmail.com>
2017-07-12 11:57:49 -0400

    adds navbar

--- mockup/src/App.js
+++ mockup/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import NavBar from './Components/NavBar';
+import { Nav, Navbar,NavItem,MenuItem,NavDropdown } from 'react-bootstrap';
 import Home from './Components/Home';
 import './App.css';
 
@@ -6,6 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <NavBar />
       <Home />
       </div>
     );
