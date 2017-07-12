d563b724e087e5b563d18c14ae0dd8458d5fc1b0
Laura Amayagit config --global user.email amaya.lauraa@gmail.com <amaya.lauraa@gmail.com>
2017-07-12 12:05:02 -0400

    adjusts header size

--- mockup/src/Components/Home.js
+++ mockup/src/Components/Home.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import projects from '../projects';
+import { tabsInstance, Tab, Tabs } from 'react-bootstrap';
+
 
 
 class Home extends Component {
@@ -20,16 +22,27 @@ componentDidMount(){
       info: projects[i].text,
       })
   }
+
   render() {
     let title = this.state.name
     let info = this.state.info
+    const tabsInstance = (
+    <Tabs defaultActiveKey={1} animation={false} id="noanim-tab-example">
+     <Tab eventKey={1} title="My Projects">Tab 1 content</Tab>
+      <Tab eventKey={2} title="Manage Applicants">Tab 2 content</Tab>
+      <Tab eventKey={3} title="Invoice Center" >Tab 3 content</Tab>
+    </Tabs>
+);
     return (
       <div className="Home">
       <section>
-      <div className="project-header">
-      <h1>{title}</h1>
-      <h1>{info}</h1>
-      </div>
+        <div className="project-header">
+          <h1>{title}</h1>
+          <h1>{info}</h1>
+         </div>
+         <div className="tabs">
+          {tabsInstance}
+         </div>
       </section>
       </div>
     );
